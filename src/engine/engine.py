3317dc83d08867a2e1e222c8149875281e2da27c
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2025-11-08 02:08:52 -0800

    smol push to engine working on engine

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -1,14 +1,30 @@
 import random
+import time
+import uuid
 from src.models import (
     Agent,
     AgentRole,
+    AIModel,
+    PolicyCard,
     PresidentPickChancellorEventPublic,
     VoteChancellorYesNoEventPublic,
     ChooseAgentToVoteOutEventPublic,
+    AskAgentIfWantsToSpeakEventPublic,
+    AgentResponseToQuestioningEventPublic,
     PresidentChooseCardToDiscardEventPrivate,
-    ChancellorPlayPolicyEventPrivate,
+    ChancellorReceivePoliciesEventPrivate,
+    ChancellorPlayPolicyEventPublic,
+    UserInput,
+    PresidentPickChancellorTool,
+    VoteChancellorYesNoTool,
+    PresidentChooseCardToDiscardTool,
+    ChancellorPlayPolicyTool,
+    AskAgentIfWantsToSpeakTool,
+    AgentResponseToQuestionTool,
 )
 from src.engine.deck import Deck
+from src.agent.agent_registry import AgentRegistry
+from src.env import settings
 
 
 class Engine:
@@ -23,7 +39,7 @@ class Engine:
         self.liberal_policies_to_win = liberal_policies_to_win
         self.hitler_election_threshold = fascist_policies_to_win // 2
 
-        self.agents: list[Agent] = []
+        self.agents_by_id: dict[str, Agent] = {}
         self.president_rotation: list[str] = []
         self.current_president_id: str | None = None
         self.current_chancellor_id: str | None = None
@@ -34,17 +50,23 @@ class Engine:
             PresidentPickChancellorEventPublic
             | VoteChancellorYesNoEventPublic
             | ChooseAgentToVoteOutEventPublic
+            | AskAgentIfWantsToSpeakEventPublic
+            | AgentResponseToQuestioningEventPublic
+            | ChancellorPlayPolicyEventPublic
         ] = []
 
         self.private_events_by_agent: dict[
             str,
             list[
                 PresidentChooseCardToDiscardEventPrivate
-                | ChancellorPlayPolicyEventPrivate
+                | ChancellorReceivePoliciesEventPrivate
             ],
         ] = {}
 
-    def create(self, agent_ids: list[str]) -> None:
+    def create(
+        self,
+        ai_models: list[AIModel | None],
+    ) -> None:
         roles = [
             AgentRole.HITLER,
             AgentRole.FASCIST,
@@ -55,15 +77,21 @@ class Engine:
 
         shuffled_roles = random.sample(roles, len(roles))
 
-        self.agents = [
-            Agent(agent_id=agent_id, role=role)
-            for agent_id, role in zip(agent_ids, shuffled_roles)
+        agent_ids = [str(uuid.uuid4()) for _ in range(len(ai_models))]
+
+        agents = [
+            Agent(agent_id=agent_id, role=role, ai_model=ai_model)
+            for agent_id, role, ai_model in zip(agent_ids, shuffled_roles, ai_models)
         ]
+
+        self.agents_by_id = {agent.agent_id: agent for agent in agents}
+
+        for agent_id in agent_ids:
+            self.private_events_by_agent[agent_id] = []
 
         self.president_rotation = random.sample(agent_ids, len(agent_ids))
 
         self.current_president_id = random.choice(agent_ids)
         self.current_chancellor_id = None
 
-        for agent_id in agent_ids:
-            self.private_events_by_agent[agent_id] = []
+    
